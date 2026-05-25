e4c36802d654a41aecc7e8cdf15694b1b4eef957
Ishan-Kumar9 <ishanraj2025@gmail.com>
2026-05-25 22:28:51 +0530

    Time: 0 ms (100%), Space: 19.4 MB (38.28%) - LeetHub

--- 2347-best-poker-hand/2347-best-poker-hand.py
+++ 2347-best-poker-hand/2347-best-poker-hand.py
@@ -1,13 +1,11 @@
 class Solution:
     def bestHand(self, ranks: List[int], suits: List[str]) -> str:
-        r = set(ranks)
-        s = set(suits)
-        if len(s) == 1:
+        if len(set(suits)) == 1:
             return "Flush"
-        for i in r:
+        for i in set(ranks):
             if ranks.count(i) > 2:
                 return "Three of a Kind"
-        for i in r:
+        for i in set(ranks):
             if ranks.count(i) == 2:
                 return "Pair"
         return "High Card"
