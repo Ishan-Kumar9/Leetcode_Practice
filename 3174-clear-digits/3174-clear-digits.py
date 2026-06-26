2e34f5c2e594bee10025e486401a1925169057f0
Ishan-Kumar9 <ishanraj2025@gmail.com>
2026-06-26 23:13:24 +0530

    Time: 0 ms (100%), Space: 19.3 MB (70.52%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.py
@@ -0,0 +1,11 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        res = []
+        for i in s:
+            if i.isalpha():
+                res.append(i)
+            else:
+                if res:
+                    res.pop()
+        ans = "".join(res)
+        return ans
