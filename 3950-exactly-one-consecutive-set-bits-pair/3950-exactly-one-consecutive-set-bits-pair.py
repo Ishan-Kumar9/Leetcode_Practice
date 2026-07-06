523d6d715311ea8e75fa87a8d279f4921962cf40
Ishan-Kumar9 <ishanraj2025@gmail.com>
2026-07-06 16:00:12 +0530

    Time: 4 ms (5.34%), Space: 19.5 MB (16.9%) - LeetHub

--- 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.py
+++ 3950-exactly-one-consecutive-set-bits-pair/3950-exactly-one-consecutive-set-bits-pair.py
@@ -1,6 +1,8 @@
 class Solution:
     def consecutiveSetBits(self, n: int) -> bool:
         b = bin(n)[2:]
-        x = b.count("11")
-        y = b.count("111")
-        return x == 1 and y == 0
+        x = 0
+        for i in range(len(b)-1):
+            if b[i] == "1" and b[i+1] == "1":
+                x += 1
+        return x == 1
