c3a082e9394596594575fa3812df922f34e2ce21
Ishan-Kumar9 <ishanraj2025@gmail.com>
2026-08-23 08:21:17 +0530

    Time: 66 ms (30.59%), Space: 19.8 MB (65.29%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sumGame(self, num: str) -> bool:
+        mid = len(num) // 2
+        left = right = 0
+        lcount = rcount = 0
+        for i in range(len(num)):
+            if i < mid:
+                if num[i].isdigit():
+                    left += int(num[i])
+                else:
+                    lcount += 1
+            else:
+                if num[i].isdigit():
+                    right += int(num[i])
+                else:
+                    rcount += 1
+        if lcount == 0 and rcount == 0:
+            return left != right
+
+        if (lcount + rcount) % 2 != 0:
+            return True
+        
+        return left - right != (rcount - lcount)*9//2
+            
